7cedc103df679c99b0368b9bb300df5d41c74525
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2025-08-03 11:00:42 +0530

    Remove slash

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const path = require("node:path");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "/pages/index.html"));
+  res.status(200).sendFile(path.join(__dirname, "pages/index.html"));
 });
 
 app.get("/:path", (req, res) => {
   if (req.params.path === "about" || req.params.path === "contact-me") {
     res
       .status(200)
-      .sendFile(path.join(__dirname, `/pages/${req.params.path}.html`));
+      .sendFile(path.join(__dirname, `pages/${req.params.path}.html`));
   } else {
-    res.status(404).sendFile(path.join(__dirname, "/pages/404.html"));
+    res.status(404).sendFile(path.join(__dirname, "pages/404.html"));
   }
 });
 
